058b34d08552dff762a21cba6796e5b38fa05f8c
User <User@User-PC>
2015-07-30 15:21:16 +0900

    2015. 07. 30

---
+++ JavaAPI/src/thread/Singleton.java
@@ -0,0 +1,13 @@
+package thread;
+
+public class Singleton {
+	private static Singleton singleton = new Singleton();
+
+	private Singleton() { // 싱글톤에서는 생성자를 private 으로 은닉화 시킨다
+		System.out.println("인스턴스 생성");
+	}
+	public static Singleton getInstance(){
+	
+		return singleton;
+	}
+}
